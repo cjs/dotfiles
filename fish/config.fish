90b7ba1a7d3e8e05ab27c08660a98fd270f973bb
Craig Steinberger <cjs@github.com>
2016-05-27 14:40:54 -0400

    Fix sourcing loops

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ source $fisher_home/config.fish
 prepend_to_path "/usr/local/sbin"
 prepend_to_path "$HOME/.dotfiles/bin"
 prepend_to_path "$HOME/bin"
+prepend_to_path "./bin"
 
 # base-16
 set base16_shell ~/.config/base16-shell/base16-eighties.dark.sh
@@ -16,7 +17,7 @@ set -x -g DOTFILES "$HOME/.dotfiles"
 set -x -g PROJECTS "$HOME/code"
 
 for i in $DOTFILES/**/*.fish
-  if echo $i |grep -vq fish.symlink
+  if echo $i |grep -vq -e 'fish.symlink' -e 'fish/functions' -e 'config.fish'
     source $i
   end
 end
