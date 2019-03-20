60eb1c5ff2479a51229871d5c57d009c6cfc63e5
Craig Steinberger <cjs@github.com>
2019-03-20 11:15:25 -0400

    more fish changes

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,13 @@
 #set fisher_config ~/.config/fisherman
 #source $fisher_home/fisher.fish
 
-
+function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
+  if test -d $argv[1]
+    if not contains $argv[1] $PATH
+      set -gx PATH "$argv[1]" $PATH
+    end
+  end
+end
 
 prepend_to_path "/usr/local/sbin"
 prepend_to_path "$HOME/.dotfiles/bin"
