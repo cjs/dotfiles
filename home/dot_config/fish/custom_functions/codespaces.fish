7eb40d6ea5fb695ef4d295770f9468ceb9ff3e91
Craig Steinberger <cjs@github.com>
2023-12-08 15:41:40 -0500

    Update home/.config/fish/custom_functions/codespaces.fish

--- home/dot_config/fish/custom_functions/codespaces.fish
+++ home/dot_config/fish/custom_functions/codespaces.fish
@@ -1,3 +1,6 @@
-function cslogs
-    less /workspaces/.codespaces/.persistedshare/creation.log
+alias cslog="less /workspaces/.codespaces/.persistedshare/creation.log"
+
+function chezmoi_refresh
+  git -C /workspaces/.codespaces/.persistedshare/dotfiles pull
+  chezmoi init --apply --source /workspaces/.codespaces/.persistedshare/dotfiles
 end
