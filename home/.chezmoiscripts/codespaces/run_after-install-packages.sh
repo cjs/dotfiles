956dde6c378c9b5b49441721529910ccd1760607
Craig Steinberger <cjs@github.com>
2023-12-09 19:30:50 -0500

    fix brew install sequence

--- home/.chezmoiscripts/codespaces/run_after-install-packages.sh
+++ home/.chezmoiscripts/codespaces/run_after-install-packages.sh
@@ -3,17 +3,19 @@
 # check if brew is installed
 if ! command -v brew &>/dev/null; then
 	echo "brew could not be found, installing"
-	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+	/bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
 		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-	exit 1
+	test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+	test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bashrc
 fi
 
 brew_install() {
-	echo "\nInstalling $1"
-	if brew list $1 &>/dev/null; then
+	printf "\nInstalling %s" "$1"
+	if brew list "$1" &>/dev/null; then
 		echo "${1} is already installed"
 	else
-		brew install $1 && echo "$1 is installed"
+		brew install "$1" && echo "$1 is installed"
 	fi
 }
 
