acdd2c92a8bd389f95cd6f5294cc8d843ef9fb02
Craig Steinberger <cjs@github.com>
2026-04-28 18:18:57 -0400

    Remove home/.chezmoiscripts/darwin/01-install-rosetta-2-macos.sh Remove home/Brewfile Add home/.aerospace.toml Add home/.config/fish/conf.d/fzf.fish Add home/.config/fish/custom_functions/homebrew.fish Add home/.config/gh/hosts.yml Add home/.config/gh/config.yml Add home/.config/github-copilot/apps.json Add home/.config/github-copilot/settings.json Add home/.config/github-copilot/versions.json Add home/.config/jrnl/jrnl.yaml Add home/.config/starship.toml Add home/.base16_theme

---
+++ home/dot_config/fish/custom_functions/homebrew.fish
@@ -0,0 +1,16 @@
+# codespaces linux brew initialized in codespaces.fish
+if test -d /opt/homebrew
+  eval (/opt/homebrew/bin/brew shellenv)
+else if test -e /usr/local/bin/brew
+  eval (/usr/local/bin/brew shellenv)
+else
+  exit 0
+end
+
+if test -d (brew --prefix)"/share/fish/completions"
+  set -gp fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+  set -gp fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
